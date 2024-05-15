f672df346e9170f79a33e090ce6e1d79589e0995
Guilherme Proença Araújo <guilhermeproencaaraujo@gmail.com>
2024-05-15 03:07:40 +0100

    first cargo project

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, cargo!");
+}
+
+//cargo new - create a new project
+//cargo build - build a project
+// cargo run - run a project
+// cargo check - check if a project runs
+
+//cargo build --release to build a optimized release version
