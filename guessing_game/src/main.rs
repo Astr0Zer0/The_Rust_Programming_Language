4437bc37b4047b0020f6dbbf25332940c5df9f59
Guilherme Proença Araújo <guilhermeproencaaraujo@gmail.com>
2024-06-02 18:33:19 +0100

    guessing game finished

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,29 +5,32 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+        let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+        println!("The secret number is: {secret_number}");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
-    let guess: u32 = guess
-    .trim()
-    .parse()
-    .expect("Please Type a number!");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue, // the underscore is a catch all value
+        };
 
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number){
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You Win!"),
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            },
+        }
     }
 }
 
